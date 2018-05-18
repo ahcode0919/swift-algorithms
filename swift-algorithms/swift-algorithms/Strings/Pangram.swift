f06b278e78cb74937f357f0a0836bf12a3dde8ac
Aaron Hinton <ahcode0919@users.noreply.github.com>
2018-05-17 22:44:05 -0700

    Add Pangram a algorithm and unit tests

---
+++ swift-algorithms/swift-algorithms/Strings/Pangram.swift
@@ -0,0 +1,32 @@
+//
+//  Pangram.swift
+//  swift-algorithms
+//
+//  Created by Aaron Hinton on 5/17/18.
+//  Copyright © 2018 No Name Software. All rights reserved.
+//
+
+import Foundation
+
+/// Check if a String is a Pangram (Contains every letter of the alphabet)
+/// Swift Coding Challenges - #9
+class Pangram {
+    private static let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    static func isPangramWithSet(_ s1: String) -> Bool {
+        let s1Set = Set(s1.lowercased())
+        return s1Set.count == s1.count
+    }
+    
+    static func isPangramWithLoop(_ s1: String) -> Bool {
+        let lowercased = s1.lowercased()
+        
+        for letter in alphabet {
+            if lowercased.contains(letter) {
+                continue
+            }
+            return false
+        }
+        return true
+    }
+}
