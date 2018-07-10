a82e73bb474b75a07874e52ed7bc0236c934fff7
Aaron Hinton <ahcode0919@users.noreply.github.com>
2018-07-10 16:32:13 -0700

    Add array first duplicate files

---
+++ swift-algorithms/swift-algorithmsTests/Arrays/ArrayFirstDuplicateTests.swift
@@ -0,0 +1,22 @@
+//
+//  ArrayFirstDuplicateTests.swift
+//  swift-algorithmsTests
+//
+//  Created by Aaron Hinton on 7/10/18.
+//  Copyright © 2018 No Name Software. All rights reserved.
+//
+
+import XCTest
+
+class ArrayFirstDuplicateTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
