936f456a003787829c5a2897316982768b50928f
Aaron Hinton <ahcode0919@users.noreply.github.com>
2018-07-10 17:44:47 -0700

    Add First not repeating character algorithm file

---
+++ swift-algorithms/swift-algorithms/Strings/FirstNotRepeatingCharacter.swift
@@ -0,0 +1,15 @@
+//
+//  FirstNotRepeatingCharacter.swift
+//  swift-algorithms
+//
+//  Created by Aaron Hinton on 7/10/18.
+//  Copyright © 2018 No Name Software. All rights reserved.
+//
+
+import Foundation
+
+// Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this
+// is what you would be asked to do during a real interview.
+
+// Given a string s, find and return the first instance of a non-repeating character in it. If there is
+// no such character, return '_'.
---
+++ swift-algorithms/swift-algorithmsTests/Strings/FirstNotRepeatingCharacterTests.swift
@@ -0,0 +1,14 @@
+//
+//  FirstNotRepeatingCharacterTests.swift
+//  swift-algorithmsTests
+//
+//  Created by Aaron Hinton on 7/10/18.
+//  Copyright © 2018 No Name Software. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class FirstNotRepeatingCharacterTests: XCTestCase {
+    
+}
